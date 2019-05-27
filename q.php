8d712b7baf8a631d99928de8996536bebb6f5b07
Ray <ray@qstudio.us>
2019-05-27 23:43:15 +0200

    2.3.6

--- q.php
+++ q.php
@@ -13,7 +13,7 @@
  * Plugin Name:     Q
  * Plugin URI:      https://www.qstudio.us
  * Description:     Q is a Development Framework that provides an API to manage libraries, themes, plugins and widgets.
- * Version:         2.3.5
+ * Version:         2.3.6
  * Author:          Q Studio
  * Author URI:      https://www.qstudio.us
  * License:         GPL
@@ -40,7 +40,7 @@ if ( ! class_exists( 'Q' ) ) {
         private static $instance = null;
 
         // Plugin Settings
-        const version = '2.3.5';
+        const version = '2.3.6';
         const text_domain = 'q-textdomain'; // for translation ##
         static $debug = false; // global debuggin ##
         static $device; // current device ##
