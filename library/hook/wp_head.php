2c392af95835adda4b1a32c55a08b5cb2bea4a0f
Ray <ray@qstudio.us>
2019-05-28 08:46:37 +0200

    Hooks

--- library/hook/wp_head.php
+++ library/hook/wp_head.php
@@ -279,9 +279,16 @@ class wp_head extends \Q {
 
 
     /**
-     * add webmaster verification meta to head ##
+     * add webmaster verification meta to head
+     * 
+     * 
+     * _deprecated
      */
     public static function webmasters() {
+
+        helper::log( 'This function is deprecated since 2.3.6' );
+
+        return false;
 
         #global $q_options; // load framework options ##
     
@@ -350,6 +357,10 @@ class wp_head extends \Q {
      * Google Analytics tracking code ##
      */
     public static function google_analytics() {
+
+        helper::log( 'This function is deprecated since 2.3.6' );
+
+        return false;
         
         #global $q_options; // load framework options ##
         
@@ -367,17 +378,17 @@ class wp_head extends \Q {
 
         if ( ! $analytics ) { return false; }
         
-        if ( class_exists( 'q_theme' ) && method_exists( 'q_theme', 'the_analytics' ) ) {
+        // if ( class_exists( 'q_theme' ) && method_exists( 'q_theme', 'the_analytics' ) ) {
 
-            // print markup ##
-            echo Q_Template::the_analytics();
+        //     // print markup ##
+        //     echo Q_Template::the_analytics();
 
-        // which template file to use ( plugin or theme ) -- TODO ##
-        } else {
+        // // which template file to use ( plugin or theme ) -- TODO ##
+        // } else {
 
-            q_get_template_part( "templates/analytics.php" );    
+        //     q_get_template_part( "templates/analytics.php" );    
         
-        }
+        // }
         
     }
     
