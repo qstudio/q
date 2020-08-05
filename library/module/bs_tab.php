9aa2ca4bc96c7b5a79627003a0f88a3c32cc8f53
RayQ <ray@qstudio.us>
2020-08-05 14:13:41 +0100

    Tabs

--- library/module/bs_tab.php
+++ library/module/bs_tab.php
@@ -33,34 +33,11 @@ class bs_tab extends \Q {
 
 		}
 		
-
-        // add assets ##
-        // \add_action( 'wp_enqueue_scripts', [ get_class(), 'wp_enqueue_scripts' ], 1000000 );
-
         // add html to footer ##
         \add_action( 'wp_footer', [ get_class(), 'wp_footer' ], 3 );
 
-        // add CSS to header ##
-        // \add_action( 'wp_head', [ get_class(), 'wp_head' ], 3 );
-
-        // add JS to footer ##
-        // \add_action( 'wp_footer', [ get_class(), 'run_javascript' ], 10000000 );
-
     }
 
-
-
-    public static function args( $args = false )
-    {
-
-        #helper::log( 'passed args to modal' );
-        // helper::log( $args );
-
-        // update passed args ##
-        self::$args = \wp_parse_args( $args, self::$args );
-
-	}
-	
 
 	/**
      * Add new libraries to Q Settings via API
@@ -70,48 +47,19 @@ class bs_tab extends \Q {
     public static function filter_acf_extension( $field )
     {
 
-        // h::log( $field['choices'] );
-        // h::log( $field['default_value'] );
-
 		// pop on a new choice ##
 		$field['choices']['bs_tab'] = 'Bootstrap Tabs';
-		// $field['choices']['banner'] = '@todo - News Banner';
 
 		// make it selected ##
 		$field['default_value'][0] = 'bs_tab';
-		
-        // h::log( $field['choices'] );
-        // h::log( $field['default_value'] );
 
-         return $field;
+		// kick back ##
+		return $field;
 
 	}
 
 
     
-    
-    /**
-    * Load assets
-    *
-    * @since    2.0.0
-    * @return   Mixed Boolean on error or HTML string
-    */
-    public static function wp_enqueue_scripts()
-    {
-
-		$min = ( true === \Q::$debug ) ? '' : '.min' ;
-
-        // toast JS ##
-        // \wp_register_script( 'toast-js', h::get( "asset/js/vendor/toast$min.js", 'return' ), array( 'jquery' ), self::version, true );
-        // \wp_enqueue_script( 'toast-js' );
-
-		// toast CSS ##
-        // \wp_register_style( 'toast-css', h::get( "asset/css/vendor/toast$min.css", 'return' ), '', self::version, 'all' );
-        // \wp_enqueue_style( 'toast-css' );
-
-    }
-
-
 
     
     /**
@@ -203,39 +151,6 @@ if( typeof jQuery !== 'undefined' ) {
 
 };
 </script>
-<?php
-
-    }
-
-
-
-    /**
-     * Deal nicely with CSS
-     */
-    public static function wp_head()
-    {
-
-        css::ob_get([
-            'view'      => get_class(), 
-            'method'    => 'css',
-            'priority'  => 40,
-            'handle'    => 'Toast'
-        ]);
-
-    }
-
-
-
-    
-    public static function css()
-    {
-
-?>
-<style>
-    .featherlight {
-        background: rgba(0,0,0,.8) !important;
-    }
-</style>
 <?php
 
     }
